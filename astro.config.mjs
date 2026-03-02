26c5c30aaf805b7e8d676ea1a55424f6e6718ba2
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2026-03-02 11:29:25 +0530

    feat: add Sanity configuration and schema for blog posts
    - Created a new Sanity configuration file (sanity.config.js) to define project settings and schema.
    - Added a PostCard component (PostCard.astro) for rendering individual blog posts.
    - Defined a schema for posts in Sanity (src/sanity/schemaTypes/post.js) with fields for title and slug.
    - Updated schema index to include the post type (src/sanity/schemaTypes/index.js).
    - Included global CSS file (global.css) to import Tailwind CSS styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    sanity({
+      projectId: "wycjyw7e",
+      dataset: "production",
+      useCdn: false,
+      apiVersion: "2026-03-02",
+      studioBasePath: "/studio",
+    }),
+    react(),
+  ],
+});
